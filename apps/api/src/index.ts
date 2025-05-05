d84a413628b14065f3a3d3cb161f51f0fbb0ab0e
vikhyathdevadiga <vikhyathvikki179@gmail.com>
2025-05-05 13:47:14 +0530

    use src folder

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,7 +1,7 @@
 import express from "express";
 const app = express();
 
-app.get("/", (req, res) => res.send("Express on Vercel"));
+app.get("/", (req, res) => res.send("Express on Vercel is ready!"));
 
 app.listen(3001, () => console.log("Server ready on port 3000."));
 
